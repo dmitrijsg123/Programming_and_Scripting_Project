ba296e84abb8f831f9bf9cd33b94f3fd9ba39ca8
dmitrijsg123 <59980795+dmitrijsg123@users.noreply.github.com>
2020-04-16 01:13:08 +0100

    Commit as per April 15th 2020

--- analysis.py
+++ analysis.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 df=pd.read_csv('Iris.csv')     # reads the csv file
 
+output_textfile = "Variables_Summary.txt"                                                                       # writing to a text file in Python reference:  https://www.geeksforgeeks.org/writing-to-file-in-python/
+file = open(output_textfile, "w")
+file.close()
 
 #FINDING SEPAL LENGTH VALUES FOR ALL IRIS TYPES - MAX, MIN, MEAN AS WELL AS MORE DETAILED INFO WITH DESCRIBE FUNCTION
 maximum = df['Sepal_length'].max()
@@ -12,10 +15,10 @@ average = df['Sepal_length'].mean()
 description = df['Sepal_length'].describe()
 
 
-print('Maximum sepal length of all Iris species is ', maximum)
-print('Minimum sepal length of all Iris species is ', minimum)
-print('Average sepal length of all Iris species is ', average)
-print('Here is some more detailed summarized information:', description)
+print('\nMaximum sepal length of all Iris species is ', maximum, file=open(output_textfile,"a"))                    # \n - new line reference: https://wtmatter.com/python-new-line/
+print('Minimum sepal length of all Iris species is ', minimum, file=open(output_textfile,"a"))
+print('Average sepal length of all Iris species is ', round(average,1), file=open(output_textfile,"a"))                 # rounding to 1 decimal function - round(my_float,1)  reference:  https://tutorialdeep.com/knowhow/round-float-to-2-decimal-places-python/
+print('Some more detailed summarized information:', round(description,1), file=open(output_textfile,"a"))
 
 #FINDING SEPAL WIDTH VALUES FOR ALL IRIS TYPES - MAX, MIN, MEAN AS WELL AS MORE DETAILED INFO WITH DESCRIBE FUNCTION
 maximum = df['Sepal_width'].max()
@@ -23,10 +26,11 @@ minimum = df['Sepal_width'].min()
 average = df['Sepal_width'].mean()
 description = df['Sepal_width'].describe()
 
-print('Maximum sepal width of all Iris species is ', maximum)
-print('Minimum sepal width of all Iris species is ', minimum)
-print('Average sepal width of all Iris species is ', average)
-print('Here is some more detailed summarized information:', description)
+
+print('\nMaximum sepal width of all Iris species is ', maximum, file=open(output_textfile,"a"))
+print('Minimum sepal width of all Iris species is ', minimum, file=open(output_textfile,"a"))
+print('Average sepal width of all Iris species is ', round(average,1), file=open(output_textfile,"a"))
+print('Some more detailed summarized information:', round(description,1), file=open(output_textfile,"a"))
 
 #FINDING PETAL LENGTH VALUES FOR ALL IRIS TYPES - MAX, MIN, MEAN AS WELL AS MORE DETAILED INFO WITH DESCRIBE FUNCTION
 maximum = df['Petal_length'].max()
@@ -34,10 +38,11 @@ minimum = df['Petal_length'].min()
 average = df['Petal_length'].mean()
 description = df['Petal_length'].describe()
 
-print('Maximum petal length of all Iris species is ', maximum)
-print('Minimum petal length of all Iris species is ', minimum)
-print('Average petal length of all Iris species is ', average)
-print('Here is some more detailed summarized information:', description)
+
+print('\nMaximum petal length of all Iris species is ', maximum, file=open(output_textfile,"a"))
+print('Minimum petal length of all Iris species is ', minimum, file=open(output_textfile,"a"))
+print('Average petal length of all Iris species is ', round(average,1), file=open(output_textfile,"a"))
+print('Some more detailed summarized information:', round(description,1), file=open(output_textfile,"a"))
 
 #FINDING PETAL WIDTH VALUES FOR ALL IRIS TYPES - MAX, MIN, MEAN AS WELL AS MORE DETAILED INFO WITH DESCRIBE FUNCTION
 maximum = df['Petal_width'].max()
@@ -45,53 +50,69 @@ minimum = df['Petal_width'].min()
 average = df['Petal_width'].mean()
 description = df['Petal_width'].describe()
 
-print('Maximum petal width of all Iris species is ', maximum)
-print('Minimum petal width of all Iris species is ', minimum)
-print('Average petal width of all Iris species is ', average)
-print('Here is some more detailed summarized information:', description)
+print('\nMaximum petal width of all Iris species is ', maximum, file=open(output_textfile,"a"))
+print('Minimum petal width of all Iris species is ', minimum, file=open(output_textfile,"a"))
+print('Average petal width of all Iris species is ', round(average,1), file=open(output_textfile,"a"))
+print('Some more detailed summarized information:', round(description,1), file=open(output_textfile,"a"))
+
+#....................................................................................................................
 
 #Iris Setosa only  - maximums for all four parameters
-import pandas as pd
-
-df=pd.read_csv('Iris.csv')                                                     
-Setosa_maximum = df[df.Species=='setosa']                                             # df[df.Column_Name==''] function allows to narrow down only to the needed rows (reference: https://youtube.com/watch?V=xvpNA7bC8cs)
-print('Maximum values for Iris Setosa flowers are: ',Setosa_maximum.max())
+df=pd.read_csv('Iris.csv')                       # reads the csv file
+Setosa_maximum = df[df.Species=='setosa']        # df[df.Column_Name==''] function allows to narrow down only to the needed rows (reference: https://youtube.com/watch?V=xvpNA7bC8cs)
+print('\nMaximum values for Iris Setosa flowers are: ', file=open(output_textfile,"a"))
+print(Setosa_maximum.max(), file=open(output_textfile,"a"))
 
 #Iris Versicolor only  - maximums for all four parameters
-df=pd.read_csv('Iris.csv')                                                   
+df=pd.read_csv('Iris.csv')                       # reads the csv file
 Versicolor_maximum = df[df.Species=='versicolor']        
-print('Maximum values for Iris Versicolor flowers are: ',Versicolor_maximum.max())
+print('\nMaximum values for Iris Versicolor flowers are: ', file=open(output_textfile,"a"))
+print(Versicolor_maximum.max(), file=open(output_textfile,"a"))
 
 #Iris Virginica only  - maximums for all four parameters
-df=pd.read_csv('Iris.csv')                                                    
+df=pd.read_csv('Iris.csv')                       # reads the csv file
 Virginica_maximum = df[df.Species=='virginica']        
-print('Maximum values for Iris Virginica flowers are: ',Virginica_maximum.max())
+print('\nMaximum values for Iris Virginica flowers are: ', file=open(output_textfile,"a"))
+print(Virginica_maximum.max(), file=open(output_textfile,"a"))
+
+#...................................................................................................
 
 #Iris Setosa only  - minimums for all four parameters
-df=pd.read_csv('Iris.csv')                                                  
+df=pd.read_csv('Iris.csv')                       # reads the csv file
 Setosa_minimum = df[df.Species=='setosa']       
-print('Minimum values for Iris Setosa flowers are: ',Setosa_minimum.min())
+print('\nMinimum values for Iris Setosa flowers are: ', file=open(output_textfile,"a"))
+print(Setosa_minimum.min(), file=open(output_textfile,"a"))
 
 #Iris Versicolor only  - minimums for all four parameters
-df=pd.read_csv('Iris.csv')                    
+df=pd.read_csv('Iris.csv')                       # reads the csv file
 Versicolor_minimum = df[df.Species=='versicolor']        
-print('Minimum values for Iris Versicolor flowers are: ',Versicolor_minimum.min())
+print('\nMinimum values for Iris Versicolor flowers are: ', file=open(output_textfile,"a"))
+print(Versicolor_minimum.min(), file=open(output_textfile,"a"))
 
 #Iris Virginica only  - minimums for all four parameters
-df=pd.read_csv('Iris.csv')              
+df=pd.read_csv('Iris.csv')                       # reads the csv file
 Virginica_minimum = df[df.Species=='virginica']        
-print('Minimum values for Iris Virginica flowers are: ',Virginica_minimum.min())
+print('\nMinimum values for Iris Virginica flowers are: ', file=open(output_textfile,"a"))
+print(Virginica_minimum.min(), file=open(output_textfile,"a"))
+
+#...................................................................................................
 
 #Iris Setosa only  - average values for all four parameters
-df=pd.read_csv('Iris.csv')                  
+df=pd.read_csv('Iris.csv')                       # reads the csv file
 Setosa_average = df[df.Species=='setosa']       
-print('Average values for Iris Setosa flowers are: ',Setosa_average.mean())
+print('\nAverage values for Iris Setosa flowers are: ', file=open(output_textfile,"a"))
+print(round(Setosa_average.mean(),2), file=open(output_textfile,"a"))          # will round to 2 decimals here
 
 #Iris Versicolor only  - average values for all four parameters
+df=pd.read_csv('Iris.csv')                       # reads the csv file
 Versicolor_average = df[df.Species=='versicolor']        
-print('Average values for Iris Versicolor flowers are: ',Versicolor_average.mean())
+print('\nAverage values for Iris Versicolor flowers are: ', file=open(output_textfile,"a"))
+print(round(Versicolor_average.mean(),2), file=open(output_textfile,"a"))
 
 #Iris Virginica only  - average values for all four parameters
-df=pd.read_csv('Iris.csv')                
+df=pd.read_csv('Iris.csv')                       # reads the csv file
 Virginica_average = df[df.Species=='virginica']        
-print('Average values for Iris Virginica flowers are: ',Virginica_average.mean())
+print('\nAverage values for Iris Virginica flowers are: ', file=open(output_textfile,"a"))
+print(round(Virginica_average.mean(),2), file=open(output_textfile,"a"))
+
+
